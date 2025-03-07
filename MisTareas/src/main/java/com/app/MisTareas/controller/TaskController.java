26ab0c915c3e792eda24a2d89a01024241cb122a
nacho-castro <ignaciocastroplanas@gmail.com>
2025-03-07 20:41:38 -0300

    deploy success

--- MisTareas/src/main/java/com/app/MisTareas/controller/TaskController.java
+++ MisTareas/src/main/java/com/app/MisTareas/controller/TaskController.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/tasks")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://mis-tareas-d40a2.web.app")
 public class TaskController {
     @Autowired
     private TaskRepository taskRepository;
